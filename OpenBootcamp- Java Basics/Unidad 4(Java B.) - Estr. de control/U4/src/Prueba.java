a7d34eea02e6e8c6a45531257da0d790cbe11fee
matias batista <matias.e.batista@hotmail.com git config --global user.email matias.e.batista@hotmail.com>
2022-10-31 17:54:43 -0300

    U4

---
+++ OpenBootcamp- Java Basics/Unidad 4(Java B.) - Estr. de control/U4/src/Prueba.java
@@ -0,0 +1,8 @@
+public class Prueba {
+    public static void main(String[ ] main ){
+        String[] nombres = {"Matias","Pablo","Maximiliano","Fabian"};
+        for(String nombre:nombres){
+            System.out.println(nombre);
+        }
+    }
+}
