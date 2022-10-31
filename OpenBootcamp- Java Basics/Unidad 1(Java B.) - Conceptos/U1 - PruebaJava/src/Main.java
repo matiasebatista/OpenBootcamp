01f940e81c9479ceea079645c390b279a6b3a67e
matias batista <matias.e.batista@hotmail.com git config --global user.email matias.e.batista@hotmail.com>
2022-10-31 17:24:20 -0300

    U2

---
+++ OpenBootcamp- Java Basics/Unidad 1(Java B.) - Conceptos/U1 - PruebaJava/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+/*                  ACA VOY A HACER RESUMEN DE LA TEORIA EXPLICADA
+*   System.out.println("");  ----> IMPRIMIR
+* @see se usa para mirar otras clases o paquetes
+*           Variables y tipos de Datos:  tipo identificador = valor;
+*              -enteros: byte number1 = 1;/short numb2=2;/ int numb3= 4; /long numb4 = 8; => = capacidad byte (long mayor capacidad)
+*              -float:  float decimal1= 4.9f;double decimal2 = 9.99;
+*               -caracter: char carac1 = 'a';
+*               - booleano: boolean verd = true;/ boolean false = false;
+*               -cadena texto:  String nomb = "Texto";
+*               -envoltorio:    Integer num = num;
+*     Los que comienzan en miniscula son tipos primitivos y no pueden ser "null" asi que se usan los envoltorios ( Mayusc. Inicial)
+*
+*           OPERADORES: ARITMETICO + - / * %(Resto) // LOGICO > < <= >= == != &&(and) ||(or) !(negación) // ASIGNACION  = += -= /= %=//
+*                       INCREMENTO ++ // DECREMENTO -- // CONCATENAR +
+*
+*           PALABRAS RESERVADAS:  ej: class list etc // clases = NombreClase // variable = nombreClase
+*
+*           FUNCIONES:  PUBLIC(visibilidad) STATIC(pertenece a la clase) VOID(tipo de dato que devuelve) nomberFunc()(identificador){}
+*                           RETORNO return  // IMPRIMIR System.out.prinln("")
+*
+*
+*      VISIBILIDAD:   public (otras clases pueden utilizarla) //private(solo en su clase)//protected(solo aquellas clases que son hijas o del mismo paquete)
+*
+*
+*
+*
+*
+*
+*    */
---
+++ OpenBootcamp- Java Basics/Unidad 2(Java B.) - IDE , archivos y sintaxis/EJ1-U2/src/Prueba/Prueba.java
@@ -0,0 +1,12 @@
+package Prueba;
+
+public class Prueba {
+    public static void main(String[] args){
+        System.out.println("Hola");
+        System.out.println(22);
+        System.out.println(1.23);
+        System.out.println(true);
+        System.out.println('a');
+    }
+
+}
